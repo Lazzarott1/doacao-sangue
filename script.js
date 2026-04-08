ca2b8a58d3413c007a3b361c38230ef029c7ccda
Lazzarott1 <lucas_lazzarotti@estudante.sesisenai.org.br>
2026-04-08 12:56:05 -0300

    add- declaração das variaveis em js

---
+++ script.js
@@ -0,0 +1,11 @@
+function validarFormulario() {
+
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let idade = document.getElementById("idade").value
+    let peso = document.getElementById("peso").value
+    let tipo = document.getElementById("tipo").value
+    let telefone = document.getElementById("telefone").value
+    let cidade = document.getElementById("cidade").value
+    let estado = document.getElementById("estado").value
+}
